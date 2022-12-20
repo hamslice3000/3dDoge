edbae59c239049e94587a357f196a2117e92ce29
hamslice3000 <106014426+hamslice3000@users.noreply.github.com>
2022-12-20 16:55:16 +0000

    Update mscript.js

--- mscript.js
+++ mscript.js
@@ -18,7 +18,7 @@
 
   function init() {
 
-    const MODEL_PATH = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1376484/stacy_lightweight.glb';
+    const MODEL_PATH = 'https://github.com/hamslice3000/3dDoge/blob/main/Mx_Mb_Export_Fixed_01.glb';
     const canvas = document.querySelector('#c');
     const backgroundColor = 0xf1f1f1;
 
